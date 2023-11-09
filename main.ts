c82ad7b7e3cc6ad121e1ca4c0c9fedbf27050a5d
Sergio Gutierrez Rojas <148484607+sergio-gr1234@users.noreply.github.com>
2023-11-09 18:17:26 +0000

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 input.onGesture(Gesture.Shake, function () {
     basic.showNumber(randint(1, 6))
     music.play(music.builtinPlayableSoundEffect(soundExpression.spring), music.PlaybackMode.UntilDone)
+    music.play(music.stringPlayable("- E D C5 C5 F D C5 ", 418), music.PlaybackMode.UntilDone)
 })
